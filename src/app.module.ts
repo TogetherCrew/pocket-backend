94cfbed2ce2a1e1f73ac4eaf3623de6e1de55987
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2023-08-20 23:56:17 +0330

    update [app module] -> append winston module and global filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,47 @@
 import { Module } from '@nestjs/common';
 import { RetrieveModule } from './retrieve/retrieve.module';
 import { AnalyticsModule } from './analytics/analytics.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { WinstonModule } from '@common/winston/winston.module';
+import { format } from 'winston';
+import LokiTransport from 'winston-loki';
+import { winstonConsoleTransport } from '@common/winston/winston.utils';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionsFilter } from '@common/filters/all-exception.filter';
 
 @Module({
-  imports: [RetrieveModule, AnalyticsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    WinstonModule.forRootAsync({
+      useFactory: (config: ConfigService) => {
+        return {
+          level: config.get<boolean>('DEBUG_MODE') ? 'debug' : 'info',
+          format: format.combine(
+            format.ms(),
+            format.timestamp(),
+            format.json(),
+          ),
+          transports:
+            config.get<string>('NODE_ENV') === 'production'
+              ? [
+                  new LokiTransport({
+                    host: config.get<string>('GRAFANA_LOKI_HOST'),
+                  }),
+                  winstonConsoleTransport,
+                ]
+              : [winstonConsoleTransport],
+        };
+      },
+      inject: [ConfigService],
+    }),
+    RetrieveModule,
+    AnalyticsModule,
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
 })
 export class AppModule {}
