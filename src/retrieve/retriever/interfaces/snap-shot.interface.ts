2d94193f52d599ed771737b7d43f7bd9460c5eef
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2023-08-28 19:12:29 +0330

    update [snapshot retriever] -> implement snapshot basic functions

--- src/retrieve/retriever/interfaces/snap-shot.interface.ts
+++ src/retrieve/retriever/interfaces/snap-shot.interface.ts
@@ -2,6 +2,10 @@ export interface SnapShotOptions {
   // TODO: implement the interface
 }
 
+export interface SnapShotResponse {
+  data: Record<string, any>;
+}
+
 export interface SnapShotOutput {
   // TODO: implement the interface
 }
--- src/retrieve/retriever/retrievers/snap-shot.retriever.ts
+++ src/retrieve/retriever/retrievers/snap-shot.retriever.ts
@@ -3,14 +3,58 @@ import { BaseRetriever } from '../interfaces/common.interface';
 import {
   SnapShotOptions,
   SnapShotOutput,
+  SnapShotResponse,
 } from '../interfaces/snap-shot.interface';
+import { WinstonProvider } from '@common/winston/winston.provider';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class SnapShotRetriever
   implements BaseRetriever<SnapShotOptions, SnapShotOutput>
 {
-  retrieve(options: SnapShotOptions): SnapShotOutput {
-    throw new Error('Method not implemented.');
-    // TODO: complete the logic
+  constructor(
+    private readonly config: ConfigService,
+    private readonly axios: HttpService,
+    private readonly logger: WinstonProvider,
+  ) {}
+
+  private async request(query: string, variables: Record<string, any>) {
+    this.logger.debug(
+      `query: ${query}\nvariables: ${variables}`,
+      SnapShotRetriever.name,
+    );
+
+    const response = await firstValueFrom(
+      this.axios.post<SnapShotResponse>(
+        this.config.get<string>('SNAP_SHOT_API_BASE_URL'),
+        {
+          query,
+          variables,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      ),
+    );
+
+    return response.data;
+  }
+
+  private getGQLQuery(): string {
+    return ``;
+  }
+
+  private serialize(response: SnapShotResponse): SnapShotOutput {
+    return {};
+  }
+  async retrieve(options: SnapShotOptions): Promise<SnapShotOutput> {
+    const query = this.getGQLQuery();
+    const raw_metrics = await this.request(query, options);
+
+    return this.serialize(raw_metrics);
   }
 }
