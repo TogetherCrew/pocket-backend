079e60400f997240d019137ba4d5afd3079ee5ac
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2023-09-14 23:04:23 +0330

    new [db module] -> implement database module

---
+++ src/common/database/database.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CoinGecko, CoinGeckoSchema } from './schemas/coin-gecko.schema';
+import {
+  CompoundMetrics,
+  CompoundMetricsSchema,
+} from './schemas/compound-metrics.schema';
+import { GoogleSheet, GoogleSheetSchema } from './schemas/google-sheet.schema';
+import { PoktScan, PoktScanSchema } from './schemas/pokt-scan.schema';
+import { SnapShot, SnapShotSchema } from './schemas/snap-shot.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_DB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forFeature([
+      { name: CoinGecko.name, schema: CoinGeckoSchema },
+      { name: CompoundMetrics.name, schema: CompoundMetricsSchema },
+      { name: GoogleSheet.name, schema: GoogleSheetSchema },
+      { name: PoktScan.name, schema: PoktScanSchema },
+      { name: SnapShot.name, schema: SnapShotSchema },
+    ]),
+  ],
+})
+export class DatabaseModule {}
