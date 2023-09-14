6702e502609620f2ce683d9dd8fbb9ceb8f959a1
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2023-09-14 23:03:29 +0330

    new [db schemas] -> create Poktscan schema

---
+++ src/common/database/schemas/pokt-scan.schema.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({
+  _id: false,
+  versionKey: false,
+  timestamps: false,
+  collection: PoktScan.name,
+})
+export class PoktScan {
+  @Prop({ unique: true })
+  date: Date;
+
+  @Prop()
+  income: number;
+
+  @Prop()
+  expense: number;
+
+  @Prop()
+  token_burn: number;
+
+  @Prop()
+  token_issuance: number;
+
+  @Prop()
+  circulating_supply: number;
+}
+
+export const PoktScanSchema = SchemaFactory.createForClass(PoktScan);
