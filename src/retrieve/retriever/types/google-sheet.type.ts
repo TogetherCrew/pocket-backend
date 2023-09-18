cb4c6af1663f0328f794cb1a1f4b9317cee82310
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2023-09-18 18:04:06 +0330

    debug [google-sheet retriever] -> correct column name

--- src/retrieve/retriever/types/google-sheet.type.ts
+++ src/retrieve/retriever/types/google-sheet.type.ts
@@ -1,4 +1,4 @@
-export type GoogleSheetColumnsName = 'Date' | 'value';
+export type GoogleSheetColumnsName = 'date' | 'value';
 
 export type GoogleSheetSerializedValues = {
   [key in GoogleSheetColumnsName]: any;
