eae0d87e98715d996b24d0c3ae2fe7061da1efc7
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2023-09-18 18:08:41 +0330

    new [retrieve module] -> implement schedule method to retrieving and storing essential and compound metrics value daily

--- src/retrieve/retrieve.module.ts
+++ src/retrieve/retrieve.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RetrieveService } from './retrieve.service';
 import { AggregationService } from './aggregation.service';
 import { RetrieverModule } from './retriever/retriever.module';
-
+import { ScheduleModule } from '@nestjs/schedule';
+import { StoreService } from './store.service';
 @Module({
-  imports: [RetrieverModule],
-  providers: [RetrieveService, AggregationService],
+  imports: [RetrieverModule, ScheduleModule.forRoot()],
+  providers: [RetrieveService, AggregationService, StoreService],
 })
 export class RetrieveModule {}
