e95c3080afcfa0c08a1d470bf94d76dff8655cc3
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2023-09-18 17:59:54 +0330

    update [database module] -> make that module global

--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CoinGecko, CoinGeckoSchema } from './schemas/coin-gecko.schema';
@@ -10,21 +10,25 @@ import { GoogleSheet, GoogleSheetSchema } from './schemas/google-sheet.schema';
 import { PoktScan, PoktScanSchema } from './schemas/pokt-scan.schema';
 import { SnapShot, SnapShotSchema } from './schemas/snap-shot.schema';
 
-@Module({
-  imports: [
-    MongooseModule.forRootAsync({
-      useFactory: (config: ConfigService) => ({
-        uri: config.get<string>('MONGO_DB_URI'),
+@Module({})
+export class DatabaseModule {
+  static forRoot(): DynamicModule {
+    return {
+      ...MongooseModule.forRootAsync({
+        useFactory: (config: ConfigService) => ({
+          uri: config.get<string>('MONGO_DB_URI'),
+        }),
+        inject: [ConfigService],
       }),
-      inject: [ConfigService],
-    }),
-    MongooseModule.forFeature([
-      { name: CoinGecko.name, schema: CoinGeckoSchema },
-      { name: CompoundMetrics.name, schema: CompoundMetricsSchema },
-      { name: GoogleSheet.name, schema: GoogleSheetSchema },
-      { name: PoktScan.name, schema: PoktScanSchema },
-      { name: SnapShot.name, schema: SnapShotSchema },
-    ]),
-  ],
-})
-export class DatabaseModule {}
+      ...MongooseModule.forFeature([
+        { name: CoinGecko.name, schema: CoinGeckoSchema },
+        { name: CompoundMetrics.name, schema: CompoundMetricsSchema },
+        { name: GoogleSheet.name, schema: GoogleSheetSchema },
+        { name: PoktScan.name, schema: PoktScanSchema },
+        { name: SnapShot.name, schema: SnapShotSchema },
+      ]),
+      module: DatabaseModule,
+      global: true,
+    };
+  }
+}
